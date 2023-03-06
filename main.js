0b8b549d936f55fa66fe464887e964bdc17351f7
saleschan <lipesalessilva2017@gmail.com>
2023-03-06 12:01:37 +0000

    electron-react-start

---
+++ main.js
@@ -0,0 +1,37 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 1000,
+    minWidth: 1200,
+    minHeight: 1000,
+    title: 'N.E.R.D',
+    icon: '',
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: true,
+    }
+  })
+
+  mainWindow.loadFile('./src/index.html')
+}
+
+require('electron-reload')(__dirname, {
+  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+})
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+const str = 'Hello World'
+
+function App() {
+  return (
+    <div>
+      {str}
+    </div>
+  );
+}
+
+export default App;
